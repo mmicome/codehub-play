8ed4551f4957e9fea6301a83e8cc4df84eea1568
mmicome <mmicome@gmail.com>
2023-04-06 20:20:17 +0800

    init

--- ai/test/ai/isPalindrome.test.ts
+++
@@ -1,7 +0,0 @@
-import { isPalindrome } from '@ai/isPalindrome';
-
-describe("isPalindrome", () => {
-    test("123454321", () => {
-        expect(isPalindrome(123454321)).toBe(true);
-    })
-})
--- ai/test/ai/pack.test.ts
+++
@@ -1,9 +0,0 @@
-import { pack } from '@ai/pack';
-
-describe("pack", () => {
-    test("pack", () => {
-        const weight = [2, 1, 5, 4, 3, 6]
-        const value = [10, 4, 7, 1, 5, 15]
-        expect(pack(6, value, weight)).toBe(19)
-    })
-});
